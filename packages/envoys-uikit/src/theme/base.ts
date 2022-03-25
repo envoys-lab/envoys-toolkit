c0b6ca4c2394d2b7df38087f6a97e2d85959d99a
Anton Shyltsev <icanton.shyltsev@americanwell.com>
2022-03-25 16:15:11 +0200

    fix card radius

--- packages/envoys-uikit/src/theme/base.ts
+++ packages/envoys-uikit/src/theme/base.ts
@@ -37,7 +37,7 @@ const spacing: Spacing = [0, 4, 8, 16, 24, 32, 48, 64];
 const radii = {
   small: "4px",
   default: "14px",
-  card: "24px",
+  card: "16px",
   circle: "50%",
   icon: "8px",
 };
