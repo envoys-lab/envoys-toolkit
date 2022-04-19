37eb9c0be859a5acc511e6c5ee3042f60ad25756
Anton Shyltsev <icanton.shyltsev@americanwell.com>
2022-04-19 23:09:27 +0300

    rollback

--- packages/envoys-uikit/src/util/isTouchDevice.ts
+++ packages/envoys-uikit/src/util/isTouchDevice.ts
@@ -1,7 +1,6 @@
 const isTouchDevice = (): boolean => {
   return (
     typeof window !== "undefined" &&
-    // @ts-expect-error: Expected navigator error
     ("ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0)
   );
 };
--- packages/envoys-uikit/src/widgets/WalletModal/WalletCard.tsx
+++ packages/envoys-uikit/src/widgets/WalletModal/WalletCard.tsx
@@ -43,7 +43,6 @@ const WalletCard: React.FC<Props> = ({ login, walletConfig, onDismiss }) => {
     <WalletButton
       variant="tertiary"
       onClick={() => {
-        // @ts-expect-error: Expected MSStream error
         const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
         // Since iOS does not support Trust Wallet we fall back to WalletConnect
